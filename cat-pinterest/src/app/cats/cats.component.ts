d8f0240c7319642dbb46ff01c3981016017b2937
Anna Navoznova <anna.navoznova@gmail.com>
2022-06-02 19:40:07 +0800

    add function to add image to favourites

--- cat-pinterest/src/app/cats/cats.component.ts
+++ cat-pinterest/src/app/cats/cats.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { forkJoin } from 'rxjs/internal/observable/forkJoin';
 
 @Component({
   selector: 'app-cats',
@@ -8,30 +7,36 @@ import { forkJoin } from 'rxjs/internal/observable/forkJoin';
   styleUrls: ['./cats.component.scss']
 })
 export class CatsComponent implements OnInit {
-  friends: Friend[] = [];
+  catsImages: Image[] = [];
+  catsImagesFavourite: Image [] = [];
 
   constructor(private httpClient: HttpClient) { 
   }
 
   ngOnInit(): void {      
-    this.getFriends();
+    this.getImages();
   }
 
-  getFriends(){
-    this.httpClient.get<any>('https://api.thecatapi.com/v1/images/search').subscribe(
+  getImages(){
+    this.httpClient.get<any>('https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc').subscribe(
       response => {
-        console.log(response);
-        this.friends = response;
-      }
-    );
+        this.catsImages = response;
+      });
   }
 
+  addToFavourites(event: any){
+    let favouriteImgUrl = (<HTMLInputElement>event.target).src;
+    let favouriteImgId = (<HTMLInputElement>event.target).alt;
+    let favouriteImg = new Image(favouriteImgId, favouriteImgUrl);
+    
+    this.catsImagesFavourite.push(favouriteImg);
+
+  }
+
+  deleteFromFavourites(event: any){
+  }
 }
 
-export class Friend {
-  constructor(
-    public id: number,
-    public url: string
-  ) {
-  }
+export class Image {
+  constructor(public id: string, public url: string) {}
 }
